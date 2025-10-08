691db2ab904cf15c5aff47a41a975526347cfcc6
cgvalle <cgvalle@uc.cl>
2025-10-08 13:30:50 -0300

    actualizado para filtar

--- grafica_tiemporeal.py
+++ grafica_tiemporeal.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from filtrado import ButterBandpassFilter
+import numpy as np
+
+filtro = ButterBandpassFilter(2, 40, fs=250, order=4)
 
 # Cargar datos (ajusta ruta si es necesario)
 df = pd.read_csv("subject01_session01_data.csv")
@@ -10,12 +14,13 @@ for i in range(1,9):
     df[f"ch{i}_uV"]=df[f"ch{i}"]*factor
 
 # Graficar los 8 canales (suponiendo nombres ch1..ch8)
+time = np.linspace(0, len(df)/250, len(df))
 plt.figure(figsize=(12, 6))
 for i in range(1, 9):
-    plt.plot(df[f'ch{i}_uV'], label=f'Canal {i}')
+    plt.plot(time,filtro.apply(df[f'ch{i}_uV']), label=f'Canal {i}')
 
 plt.legend()
 plt.title("Señales EMG de los 8 canales")
-plt.xlabel("Muestras")
+plt.xlabel("Tiempo (s)")
 plt.ylabel("Voltaje (µV)")
 plt.savefig("emg_canales.png")
