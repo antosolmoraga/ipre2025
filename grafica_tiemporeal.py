801d795760698c46ef871f50136381d7130728a5
antosolmoraga <antomoraga@uc.cl>
2025-10-08 20:13:15 -0300

    “data”

--- grafica_tiemporeal.py
+++ grafica_tiemporeal.py
@@ -2,25 +2,49 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from filtrado import ButterBandpassFilter
 import numpy as np
+from scipy.signal import butter, filtfilt
 
-filtro = ButterBandpassFilter(2, 40, fs=250, order=4)
-
-# Cargar datos (ajusta ruta si es necesario)
-df = pd.read_csv("subject01_session01_data.csv")
-
+FS=250
+filtro = ButterBandpassFilter(20, 80, fs=FS, order=4)
 factor=(4.5/((2**23-1)*24))*16
+
+# Cargar datos
+df = pd.read_csv("subject01_session04_data.csv")
+
 
 for i in range(1,9):
     df[f"ch{i}_uV"]=df[f"ch{i}"]*factor
 
-# Graficar los 8 canales (suponiendo nombres ch1..ch8)
-time = np.linspace(0, len(df)/250, len(df))
-plt.figure(figsize=(12, 6))
-for i in range(1, 9):
-    plt.plot(time,filtro.apply(df[f'ch{i}_uV']), label=f'Canal {i}')
+def butter_lowpass_filter(data, cutoff, fs, order=4):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype="low", analog=False)
+    return filtfilt(b, a, data)
+
+
+# --- Preprocesamiento y graficado ---
+time = np.linspace(0, len(df) / FS, len(df))
+plt.figure(figsize=(10, 4))
+
+
+# Filtrado pasa banda
+filtered = filtro.apply(df[f"ch{2}_uV"])
+
+# Rectificación
+rectified = filtered**2
+
+# Normalización entre 0 y 1
+normalized = (rectified - np.min(rectified)) / (np.max(rectified) - np.min(rectified))
+
+smoothed= butter_lowpass_filter(normalized, cutoff=10, fs=FS)
+
+# Graficar
+plt.plot(time, smoothed + 2*2, label=f'Canal {2}')  
+# ↑ sumo i*2 para desplazar verticalmente cada canal y que no se superpongan
+
 
 plt.legend()
-plt.title("Señales EMG de los 8 canales")
+plt.title("Señales EMG canal 2")
 plt.xlabel("Tiempo (s)")
 plt.ylabel("Voltaje (µV)")
-plt.savefig("emg_canales.png")
+plt.savefig("emg_canal2_procesadas.png")
