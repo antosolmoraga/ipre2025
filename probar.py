ddedc6950ac0655fcae44d1b873fd0b359094ca9
antosolmoraga <antomoraga@uc.cl>
2025-12-05 12:38:48 -0300

    eliminando

--- probar.py
+++
@@ -1,7 +0,0 @@
-from pyOpenBCI import OpenBCICyton
-
-def print_data(sample):
-    print(sample.channels_data)
-
-board = OpenBCICyton(port="/dev/ttyUSB0", daisy=False)
-board.start_stream(print_data)
--- prueba.py
+++
@@ -1,9 +0,0 @@
-from pyOpenBCI import OpenBCICyton
-
-def callback(sample):
-    print(sample.channels_data)  # lista de 8 valores
-    return  # para no bloquear
-
-board = OpenBCICyton(port="/dev/ttyUSB0", daisy=False)
-print("Streaming...")
-board.start_stream(callback)
